d011f3af5194ce4403dd731fe6c584f34496f279
JoydeepK <JoydeepK@SKYDELL-12-05>
2018-05-14 11:50:08 +0530

    Add project files.

---
+++ Asopalav/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asopalav.Startup))]
+namespace Asopalav
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
