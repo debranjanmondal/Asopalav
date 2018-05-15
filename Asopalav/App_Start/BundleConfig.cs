c2494350d8393da5c9e4bd704d6a5de9f351d2d3
DebranjanM <debranjanm@gmail.com>
2018-05-15 18:46:40 +0530

    Design commit

--- Asopalav/App_Start/BundleConfig.cs
+++ Asopalav/App_Start/BundleConfig.cs
@@ -21,11 +21,15 @@ namespace Asopalav
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/jquery-1.11.1.min.js",
+                      "~/Scripts/main.js",
+                      "~/Scripts/plugins.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
         }
     }
 }
